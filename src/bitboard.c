fe7fcd4b619d88d7cd979deb87d2a317b436ac0e
Anand Mahamuni <m.anand7203@gmail.com>
2024-09-10 23:35:32 +0530

    Init. bitboards!

---
+++ src/bitboard.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef unsigned long long u64;
+
+u64 setBit(int bit) {
+  return 0;
+  // sets the square, marking a position on the  board, input type yet
+  // undecided!
+}
+
+int main() {
+  const u64 t = 0xffffffffffffffff;
+
+  printf("%llu", t);
+  return 0;
+}
